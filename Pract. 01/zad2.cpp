cf0074af9e6231c4165703e70135bc841e550bdf
Georgi Iliev <georgi.iliev533@outlook.com>
2022-10-05 19:47:59 +0300

    Update zad2.cpp
    Big bruh moment

--- Pract. 01/zad2.cpp
+++ Pract. 01/zad2.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main() {
-    int a = 5.4;
-    int b = 7.9;
+    double a = 5.4;
+    double b = 7.9;
 
     cout << "P = " << 2 * a + 2 * b << endl;
     cout << "S = " << a * b << endl;
