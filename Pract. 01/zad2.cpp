65fa15a5b91432ef0eab7b15db5ecd815ba86ffd
Georgi Iliev <georgi.iliev533@outlook.com>
2022-10-05 18:59:26 +0300

    Zadachi

---
+++ Pract. 01/zad2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 5.4;
+    int b = 7.9;
+
+    cout << "P = " << 2 * a + 2 * b << endl;
+    cout << "S = " << a * b << endl;
+    return 0;
+
+}
