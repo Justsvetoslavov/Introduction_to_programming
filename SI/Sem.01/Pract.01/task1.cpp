5af9c89c1d42cf161587f7d2de5f30e0eb7e341f
Georgi Baltiev <82998942+georgibaltiev@users.noreply.github.com>
2022-10-12 18:57:53 +0300

    Add files via upload
    test task 1 (после се очакват и другите)

---
+++ SI/Sem.01/Pract.01/task1.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Oh what" << endl << "a happy day!" << endl << "Oh yes," << endl << "what a happy day!" << endl;
+   return 0;
+}
