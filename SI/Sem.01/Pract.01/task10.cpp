5caff417b4c9414e27e3ac074e94d3cc89897aa8
militsa_toncheva <103645079+milicavilica@users.noreply.github.com>
2022-10-12 19:17:18 +0300

    task10_solution.cpp
    :)

--- SI/Sem.01/Pract.01/task10.cpp
+++ SI/Sem.01/Pract.01/task10.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-	int a, b;
+/*	int a, b;
 	cout << "a: ";
 	cin >> a;
 	cout << "b: ";
@@ -17,5 +17,22 @@ int main()
 	cout << "a: " << a << endl;
 	cout << "b: " << b << endl;
 
+	return 0; */
+	
+	//10
+    double ak, bk, ck;
+    cout << "a = ";
+    cin >> ak;
+    cout << "b = ";
+    cin >> bk;
+    cout << "c = ";
+    cin >> ck;
+
+    double x1, x2;
+    x2 = ( - bk + sqrt(bk * bk - 4 * ak * ck)) / 2 * ak;
+    x1 = (- bk - sqrt(bk * bk - 4 * ak * ck))  / 2 * ak;
+
+    cout << "x1 = " << x1 << ", x2 = " << x2 << "\n\n";
+	
 	return 0;
 }
