9f205788049f18852fe4d7cb8f3ac2099401d62b
DrHristov <the_doktora@abv.bg>
2022-10-17 16:18:28 +0300

    Updated Task03
    Will I get scolded for returning in the if? Or do you want me to use else? Actually I am genuinely curious which approach is better.

--- SI/Sem.02/Pract.02/Task03.cpp
+++ SI/Sem.02/Pract.02/Task03.cpp
@@ -8,6 +8,11 @@ int main() {
 	std::cout << "Enter second number value: ";
 	std::cin >> number2;
 
+	if (number1 == number2) {
+		std::cout << "Both numbers are equal." << std::endl;
+		return 0;
+	}
+
 	std::cout << (number1 > number2 ? "The first number is larger than the second number." : "The second number is larger than the first number.") << std::endl;
 
 	return 0;
