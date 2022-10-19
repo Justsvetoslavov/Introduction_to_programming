405f8f7697de1dd682e6085c020f85ab05be8892
Daniel Jordanov <danielgo_nz@abv.bg>
2022-10-19 19:01:28 +0300

    Task01

---
+++ SI/Sem.03/Pract.03/Task01.cpp
@@ -0,0 +1,15 @@
+// Task 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //task one
+    
+    std::cout << "Oh what"<<std::endl
+        <<"a happy day!" << std::endl
+        << "Oh yes," << std::endl
+        << "what a happy day!";
+}
