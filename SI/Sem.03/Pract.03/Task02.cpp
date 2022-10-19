d401fb96aa19358c27614c6bac78cd3cb07d5678
Daniel Jordanov <danielgo_nz@abv.bg>
2022-10-19 19:01:34 +0300

    Task02

---
+++ SI/Sem.03/Pract.03/Task02.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double a = 5.4;
+	double b = 7.9;
+	double s = a * b;
+	double p = 2 * a + 2 * b;
+	cout << "The surface of the rectangle is " << s << endl;
+	cout << "The perimeter of the rectangle is " << p;
+}
