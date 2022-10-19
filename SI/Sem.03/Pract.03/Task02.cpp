7adade70ce4db035bcd777019ba75501e5279255
momouzunov2003 <uzunovvv03@gmail.com>
2022-10-19 18:49:08 +0300

    Create Task02.cpp

---
+++ SI/Sem.03/Pract.03/Task02.cpp
@@ -0,0 +1,15 @@
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int x, y;
+    for (int i = 1; i < n; i++)
+    {
+        cout << "x= " << i << " " << "y= " << n - i << endl;
+    }
+}
+
