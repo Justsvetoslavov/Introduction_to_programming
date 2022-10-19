0961342be23bda87a5ad65c8200b9dd49ea0645b
DrHristov <the_doktora@abv.bg>
2022-10-19 18:56:01 +0300

    Create Task01.cpp

---
+++ SI/Sem.03/Pract.03/Task01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+	int n = 0;
+
+	std::cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= i; j++) {
+			std::cout << j;
+		}
+		std::cout << std::endl;
+	}
+
+	return 0;
+}
