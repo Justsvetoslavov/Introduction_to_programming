7217722619bb30367a5496cfe9757cfe679faf94
Daniel Jordanov <danielgo_nz@abv.bg>
2022-10-19 19:01:40 +0300

    Task03

---
+++ SI/Sem.03/Pract.03/Task03.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double levs = 0;
+	cin >> levs;
+	double dollars = levs * 1.981737;
+	double euro = levs * 1.9558;
+	cout << "Dollars: " << dollars << endl;
+	cout << "Euro: " << euro;
+}
