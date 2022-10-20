8fce49db137ccd8a04093a98c05b9a43eb9af259
Hari <hari.hristov.03@gmail.com>
2022-10-20 11:38:48 +0300

    Create Task12.cpp

---
+++ SI/Sem.03/Pract.03/Task12.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n = 0;
+	cin >> n;
+	bool power = true;
+
+	while (n / 2 != 1)
+	{
+		
+		if (n % 2 != 0)
+		{
+			power = false;
+		}
+
+		n /= 2;
+	}
+
+	cout << boolalpha << power;
+}
