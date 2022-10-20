369677c9b4f4eda684f4c152a765b1c6e7fa06bd
Hari <hari.hristov.03@gmail.com>
2022-10-20 11:38:10 +0300

    Create Task11.cpp

---
+++ SI/Sem.03/Pract.03/Task11.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+    cin >> a;
+    cin >> b;
+    int bigger = a >= b ? a : b;
+    int smaller = a >= b ? b : a;
+
+    for (int i = smaller; i <= bigger; i++)
+    {
+        bool isPrime = true;
+        if (i == 0 || i == 1)
+        {
+            continue;
+        }
+
+        for (int j = 2; j < i / 2; j++)
+        {
+            if (i % j == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime)
+        {
+            cout << i << " ";
+        }
+    }
+}
