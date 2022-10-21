2a787f614e00aa698b245e1467f0e76d5a3ce289
kokopeevski <koko_peevski@abv.bg>
2022-10-21 10:54:54 +0300

    Create Task03.cpp

---
+++ SI/Sem.03/Pract.03/Task03.cpp
@@ -0,0 +1,23 @@
+﻿// Task03.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Enter a number: ";
+	int n;
+	cin >> n;
+
+	int res = 0;
+	int square = n * n;
+	while (square != 0)
+	{
+		int lastDigit = square % 10;
+		square /= 10;
+
+		res = res * 10 + lastDigit;
+	}
+	cout << res;
+}
