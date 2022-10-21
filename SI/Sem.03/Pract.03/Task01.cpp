698b0be13f28d0feabbc49d9ecb0ace1cccfd847
kokopeevski <koko_peevski@abv.bg>
2022-10-21 10:54:43 +0300

    Create Task01.cpp

---
+++ SI/Sem.03/Pract.03/Task01.cpp
@@ -0,0 +1,21 @@
+﻿// Task01.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Enter a number: ";
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			cout << j;
+		}
+		cout << endl;
+	}
+}
+
