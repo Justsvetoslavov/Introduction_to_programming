f64ca853835238b07fa3500db523091ec557d377
Margarita-Gencheva2003 <oldshaturhand@gmail.com>
2022-10-22 11:07:07 +0300

    Create Task04.cpp

---
+++ SI/Sem.03/Pract.03/Task04.cpp
@@ -0,0 +1,13 @@
+﻿//#include <iostream>
+//#include <iomanip>
+//int main() {
+//	
+//	int count = 1, m = 0, n = 0;
+//	std::cin >> m >> n;
+//	for (int i = 1; i <= m; i++) {
+//		for (int j = 1; j <= n; j++) {
+//			std::cout << count++ << std::setw(3);
+//		}
+//		std::cout << std::endl;
+//	}
+//}
