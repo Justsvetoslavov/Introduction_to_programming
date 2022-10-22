b6e8aed912b22c1c3c411740825ebccd287f9a98
Atanas Hristov <the_doktora@abv.bg>
2022-10-22 19:00:49 +0300

    Refactored Task04.cpp
    Does this count as refactoring? Or do I have to do something else?

--- SI/Sem.02/Pract.02/Task04.cpp
+++ SI/Sem.02/Pract.02/Task04.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
 
 int main() {
-	int percentage = 0, grade = 2;
+	int percentage = 0, grade = 0;
 
 	std::cout << "Result: ";
 	std::cin >> percentage;
 
 	if (percentage >= 0 && percentage <= 100) {
-		if (percentage >= 40) {
+		bool isSixPlus = false;
+
+		if (percentage >= 90) {
+			grade = 6;
+			isSixPlus = true;
+		} else if (percentage >= 80) {
+			grade = 6;
+		} else if (percentage >= 70) {
+			grade = 5;
+		} else if (percentage >= 60) {
+			grade = 4;
+		} else if (percentage >= 40) {
 			grade = 3;
-			if (percentage >= 60) {
-				grade = 4;
-				if (percentage >= 70) {
-					grade = 5;
-					if (percentage >= 80) {
-						grade = 6;
-						if (percentage >= 90) {
-							std::cout << "Grade: " << grade << "+\n";
-							return 0;
-						}
-					}
-				}
-			}
+		} else {
+			grade = 2;
 		}
 
-		std::cout << "Grade: " << grade << std::endl;
+		std::cout << "Grade: " << grade << (isSixPlus ? "+\n" : "\n");
 	} else {
 		std::cout << "Entered percentage is not valid!\n";
 	}
