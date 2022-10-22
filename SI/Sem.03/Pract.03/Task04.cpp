e0959d66069b519f37d327353df02686f105eba8
Atanas Hristov <the_doktora@abv.bg>
2022-10-22 19:31:27 +0300

    Update Task04.cpp
    Removed product variable.
    Added condition for positive n and m.

--- SI/Sem.03/Pract.03/Task04.cpp
+++ SI/Sem.03/Pract.03/Task04.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 
 int main() {
-	int n = 0, m = 0, product = 0;
+	int n = 0, m = 0;
 
 	std::cin >> n >> m;
 
-	product = n * m;
-
-	for (int i = 1; i <= product; i++) {
-		std::cout << i << " ";
-		if (i % m == 0) std::cout << std::endl;
+	if (n > 0 && m > 0) {
+		for (int i = 1; i <= n * m; i++) {
+			std::cout << i << " ";
+			if (i % m == 0) std::cout << std::endl;
+		}
+	} else {
+		std::cout << "Invalid input." << std::endl;
 	}
-
 	return 0;
 }
