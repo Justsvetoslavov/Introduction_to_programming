8b398850b0f18f683b58c71e163b35a930fb7213
militsa_toncheva <mili.toncheva@icloud.com>
2022-10-22 14:58:14 +0300

    Task 11

---
+++ SI/Sem.03/Pract.03/Task11.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cout << "Вход: ";
+	cin >> a >> b;
+	int maxNum = max(a, b);
+	int minNum = min(a, b);
+	cout << "Изход: ";
+	for (int i = minNum; i <= maxNum; i++)
+	{
+		bool isPrime = true;
+		int n = i;
+		for (int j = 2; j <= n / 2; j++)
+		{
+			if (n % j == 0)
+			{
+				isPrime = false;
+				break;
+			}
+		}
+		if (isPrime && n != 1)
+		{
+			cout << n << ' ';
+		}
+	}
+}
