d04ba0d84909659fb490b39bd78a165da3ca6d80
Margarita-Gencheva2003 <oldshaturhand@gmail.com>
2022-10-22 11:07:11 +0300

    Create Таsк05.cpp

---
+++ SI/Sem.03/Pract.03/Таsк05.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//int main() {
+//	int number = 1, testNum = 0, count = 0;
+//	std::cout << "Input number between 1 and 100: ";
+//	std::cin >> number;
+//	if (number >= 1 && number <= 100) {
+//		do {
+//			std::cin >> testNum;
+//			if (testNum > number) {
+//				std::cout << testNum << " > " << std::endl;
+//			}
+//			if (testNum < number) {
+//				std::cout << testNum << " < " << std::endl;
+//			}
+//			count++;
+//		} while (testNum != number);
+//		std::cout <<"Count: " << count << std::endl;
+//	}
+//	else {
+//		std::cout << "Wrong number!" << std::endl;
+//	}
+//}
