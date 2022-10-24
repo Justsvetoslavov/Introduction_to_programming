aab0c7edf757838bb23ddea528dc90d86db8c525
Atanas Hristov <the_doktora@abv.bg>
2022-10-24 22:07:01 +0300

    Update Task07.cpp
    Removed the unnecessary check for the triangle side rule.

--- SI/Sem.03/Pract.03/Task07.cpp
+++ SI/Sem.03/Pract.03/Task07.cpp
@@ -8,7 +8,7 @@ int main() {
 	for (int i = 1; i < n; i++) {
 		for (int j = i; j < n; j++) {
 			for (int k = j; k < n; k++) {
-				if (i + j > k && i + k > j && j + k > i && (i * i + j * j == k * k)) {
+				if (i * i + j * j == k * k) {
 					std::cout << i << " " << j << " " << k << std::endl;
 				}
 			}
