30f2f06814059fe25f0d11950d4e44f58e43d7e6
Nikola Svetoslavov <justsvetoslavov@gmail.com>
2022-10-25 21:17:10 +0300

    Refactor
    - remove unnecessary casting.
    - rename variable.

--- SI/Sem.02/Pract.02/Task07.cpp
+++ SI/Sem.02/Pract.02/Task07.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
 
 int main() {
-	char input;
+	char symbol;
+	std::cin >> symbol;
 
-	std::cin >> input;
-
-	if (input >= 'A' && input <= 'Z') {
-		std::cout << char(int(input) + 32) << std::endl;
-	}
-	else if (input >= 'a' && input <= 'z') {
-		std::cout << char(int(input) - 32) << std::endl;
-	}
-	else {
+	if (symbol >= 'A' && symbol <= 'Z') {
+		std::cout << (symbol += 32) << std::endl;
+	} else if (symbol >= 'a' && symbol <= 'z') {
+		std::cout << (symbol -= 32) << std::endl;
+	} else {
 		std::cout << "Invalid character" << std::endl;
 	}
 
