7ee433cc566713ca56068e591a7adcd739ed1560
Radina Ovcharova <93722607+radi676@users.noreply.github.com>
2022-10-26 23:35:47 +0300

    Create Task03.cpp

---
+++ SI/Sem.04/Pract.04/Task03.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+void isPositive(int number) {
+	if (number>0)
+	{
+		std::cout << "Your number is positive."<<std::endl;
+	}
+	else {
+
+		std::cout << "Your number is not positive." << std::endl;
+	}
+}
+
+void isNegative(int number) {
+	if (number < 0)
+	{
+		std::cout << "Your number is negative." << std::endl;
+	}
+	else {
+
+		std::cout << "Your number is not negative."<<std::endl;
+	}
+}
+int main()
+{
+	int number = 0;
+	std::cin >> number;
+	isPositive(number);
+	isNegative(number);
+}
