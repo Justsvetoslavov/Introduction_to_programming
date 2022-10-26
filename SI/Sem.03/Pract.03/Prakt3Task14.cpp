ac171c36f8fa0f6672d9fb5384b804148c0e9ebc
stefka04 <stefka1804@gmail.com>
2022-10-27 01:18:30 +0300

    Create Task14

---
+++ SI/Sem.03/Pract.03/Prakt3Task14.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Enter a, b, c, d, e: ";
+	int a, b, c, d, e;
+	cin >> a >> b >> c >> d >> e;
+	for (int i = -100; i <= 100; i++)
+	{
+		if (a * pow(i, 4) + b * pow(i, 3) + c * pow(i, 2) + d * i + e >= 0)
+		{
+			cout << i << " ";
+		}
+	}
+	return 0;
+}
