abc11db9e381341bb2756208cbee2125bf9498b8
Radina Ovcharova <93722607+radi676@users.noreply.github.com>
2022-10-26 23:35:43 +0300

    Create Task02.cpp

---
+++ SI/Sem.04/Pract.04/Task02.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+
+int isEven(int num) {
+	if (num % 2==0)
+	{
+		std::cout << "My number is even.";
+	}
+	else {
+		std::cout << "My number is not even.";
+	}
+	return num;
+
+}
+int main()
+{
+	int num = 0;
+	std::cin >> num;
+	std::cout << isEven(num);
+}
+ //Да се напише функция isEven(int num), която връща дали дадено число е четно или не.
