63abe431b2225d40f456f3300007f600a5485251
Radina Ovcharova <93722607+radi676@users.noreply.github.com>
2022-10-26 23:35:33 +0300

    Create Task1.cpp

---
+++ SI/Sem.04/Pract.04/Task1.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+int sum(int a, int b) {
+    return a + b;
+
+ }
+int main()
+{
+    int number1 = 0;
+    int number2 = 0;
+    std :: cin >> number1 >> number2;
+    std::cout << sum(number1, number2);
+    return 0;
+}
+
+//Да се състави функция sum(int a, int b), 
+//която приема две числа и връща като резултат сбора им.
