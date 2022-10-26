dd0ecb717f641f52866dc6b9fdfddd4e49735263
militsa_toncheva <mili.toncheva@icloud.com>
2022-10-26 15:12:59 +0300

    Create Task 10.cpp

---
+++ SI/Sem.03/Pract.03/Task 10.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	int digitCounter = 1;
+	int temporaryN = n;
+	while (temporaryN / 10 != 0)
+	{
+		digitCounter++;
+		temporaryN /= 10;
+	}
+	
+	int firstNum = 1;
+	int secondNum = 1;
+	int digitCounterHalf = digitCounter / 2;
+
+	if (digitCounter % 2 == 0) digitCounterHalf -= 1;
+	for (int i = 0; i < digitCounter / 2 + 1; i++)
+	{
+		firstNum *= 10;
+	}
+	for (int i = 0; i < digitCounterHalf; i++)
+	{
+		secondNum *= 10;
+	}
+	firstNum = n / firstNum;
+	secondNum = n % secondNum;
+
+	for (int i = 0; i < digitCounterHalf; i++)
+	{
+		firstNum *= 10;
+	}
+	int wantedNumber = firstNum + secondNum;
+	cout << wantedNumber << ", " << wantedNumber + 1;
+}
