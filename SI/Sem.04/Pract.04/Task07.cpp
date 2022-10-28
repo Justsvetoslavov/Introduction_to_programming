835182500c7b7f3645c1b1921c9f483ef1a3a849
kokopeevski <koko_peevski@abv.bg>
2022-10-28 11:05:15 +0300

    Add Task07

---
+++ SI/Sem.04/Pract.04/Task07.cpp
@@ -0,0 +1,23 @@
+﻿// Task07.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int askUser(int from, int to)
+{
+	int number;
+	cout << "Enter a number: ";
+	cin >> number;
+	while (!(number >= from && number <= to))
+	{
+		cout << "Enter a new number: ";
+		cin >> number;
+	}
+	return number;
+}
+
+int main()
+{
+	cout << "The number is: " << askUser(10, 30) << endl;
+}
