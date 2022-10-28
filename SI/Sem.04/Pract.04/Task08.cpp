df04545d4493e4fd3b3262bd12a7e8792766b7ed
kokopeevski <koko_peevski@abv.bg>
2022-10-28 11:05:49 +0300

    Add Task08

---
+++ SI/Sem.04/Pract.04/Task08.cpp
@@ -0,0 +1,18 @@
+﻿// Task08.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+long long pow(int num, unsigned int N)
+{
+	for (int i = 1; i <= N; i++)
+	{
+		return num * N;
+	}
+}
+
+int main()
+{
+	cout << pow(2,7);
+}
