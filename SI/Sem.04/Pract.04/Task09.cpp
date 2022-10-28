04d525f8c992a2e68e16ceb51e5f15f84c8d2162
kokopeevski <koko_peevski@abv.bg>
2022-10-28 11:05:59 +0300

    Add Task09

---
+++ SI/Sem.04/Pract.04/Task09.cpp
@@ -0,0 +1,44 @@
+﻿// Task09.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int calculate(int a, int b, char action = '+')
+{
+	switch (action)
+	{
+		case '+':
+			return a + b;
+			break;
+		case '-':
+			return a - b;
+			break;
+		case '*':
+			return a * b;
+			break;
+		case '/':
+			if (b == 0)
+			{
+				cout << "The devisor can not be zero! Please input other number!";
+				cin >> b;
+			}
+			return a / b;
+			break;
+		case '%':
+			if (b == 0)
+			{
+				cout << "The devisor can not be zero! Please input other number!";
+				cin >> b;
+			}
+			return a % b;
+			break;
+		default:
+			break;
+	}
+}
+
+int main()
+{
+	cout << calculate(20, 5,'/');
+}
