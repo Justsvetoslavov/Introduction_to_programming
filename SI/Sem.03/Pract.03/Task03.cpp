b8c415bbc180b008fca2bc8f493e1b94c6c9a75b
Silvana Nikolova <78743124+Silvana03@users.noreply.github.com>
2022-10-28 09:40:29 +0300

    Create Task03.cpp

---
+++ SI/Sem.03/Pract.03/Task03.cpp
@@ -0,0 +1,28 @@
+﻿//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//
+//	int squareN = n * n;
+//	int resultNum = 0;
+//
+//	while (squareN > 0)
+//	{
+//		int currNum = squareN % 10;
+//		squareN /= 10;
+//
+//		if (currNum != 0)
+//		{
+//			resultNum += currNum;
+//			resultNum *= 10;
+//		}
+//	}
+//
+//	resultNum /= 10;
+//
+//	cout << resultNum;
+//
+//}
