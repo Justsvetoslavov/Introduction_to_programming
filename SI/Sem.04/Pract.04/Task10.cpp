c02aa9168350c1588da0385fb2a6cfef0bf95feb
stefka04 <stefka1804@gmail.com>
2022-10-29 19:14:22 +0300

    Create Task10

---
+++ SI/Sem.04/Pract.04/Task10.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int binarySystem(int num)
+{
+    int binaryNum = 0; 
+    int i = 1;
+    while (num > 0)
+    {
+       binaryNum = binaryNum + (num % 2)*i;
+        num /= 2;
+        i = i * 10;
+    }
+    return binaryNum;
+}
+int main()
+{
+    int num;
+    cout << "Enter a number:";
+    cin >> num;
+    cout << binarySystem(num);
+    return 0;
+}
+
