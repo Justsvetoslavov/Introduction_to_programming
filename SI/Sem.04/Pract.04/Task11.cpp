22a12154460f6b84664b96d19097b43fb892fafa
stefka04 <stefka1804@gmail.com>
2022-10-29 19:14:53 +0300

    Create Task11

---
+++ SI/Sem.04/Pract.04/Task11.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int GCD(int a,int b)
+{
+    if (b > a)
+    {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+    while (b > 0)
+    {
+        int current = a % b;
+        a = b;
+        b = current;
+    }
+    return a;
+}
+int main()
+{
+    int a, b, c, d;
+    cout << "Enter a, b, c, d:";
+    cin >> a >> b >> c >> d;
+    cout << "GCD of a and b is:" << GCD(a, b) << endl;
+    cout << "GCD of c and d is:" << GCD(c, d) << endl;
+    cout << "GCD of a, b, c and d is:" << GCD(GCD(a, b), GCD(c, d));  
+    return 0;
+}
+
