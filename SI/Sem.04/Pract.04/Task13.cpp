dfe5552fff90bcd50babe2bbec5c5083e8c40b4a
Kristian2109 <kristian.petrov1998@gmail.com>
2022-10-30 11:50:36 +0200

    Task 13
    Improvements

--- SI/Sem.04/Pract.04/Task13.cpp
+++ SI/Sem.04/Pract.04/Task13.cpp
@@ -2,49 +2,43 @@
 using namespace std;
 
 bool checkPrime(int input) {
-	bool result = true;
 	for (int i = 2; i < input; i++) {
 		if (input % i == 0) {
-			result = false;
-			return result;
+			return false;
 		}
 	}
 
-	return result;
+	return true;
 }
 
 void exportPrimes(int number) {
-	for (int lower = 1; lower < number; lower++) {
-		bool isPrime = checkPrime(lower);
+	cout << "The primes are: " << endl;
+	for (int i = 2; i < number; i++) {
+		bool isPrime = checkPrime(i);
 		if (isPrime) {
-			cout << lower << endl;
+			cout << i << endl;
 		}
 	}
-
-	return;
 }
 
-bool findSum(int a) {
-	bool result = false;
-	for (int i = 1; i <= a / 2; i++) {
+bool isSum(int numberToTest) {
+	for (int i = 1; i <= numberToTest / 2; i++) {
 		int firstNumber = i - 1;
 		if (checkPrime(firstNumber) && checkPrime(i)) {
-			result = true;
-			break;
+			return true;
 		}
 	}
 
-	return result;
+	return false;
 }
 
 int main()
 {
-
 	int a;
 	cin >> a;
 
-	cout << findSum(a);
+	cout << isSum(a) << endl;
+	exportPrimes(a);
 
 	return 0;
-
 }
