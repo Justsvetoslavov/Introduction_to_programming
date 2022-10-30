f860ac2eec15f94179c385af5172e7f45f1cf434
stefka04 <stefka1804@gmail.com>
2022-10-30 10:43:30 +0200

    Update Task09

--- SI/Sem.04/Pract.04/Task09.cpp
+++ SI/Sem.04/Pract.04/Task09.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 int calculate(int a, int b, char action)
 {    
+        if (b == 0 && action == '/')
+        {
+            cout << "Incorrect input!"<<endl;
+            return 0;
+        }
         switch (action)
         {
         case '-': return a - b; break;
