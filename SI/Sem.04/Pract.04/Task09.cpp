9cd9c8298e9c94996c7c1f69224f83f500e56420
Kristian2109 <kristian.petrov1998@gmail.com>
2022-10-30 12:07:34 +0200

    Task09
    Save improvements

--- SI/Sem.04/Pract.04/Task09.cpp
+++ SI/Sem.04/Pract.04/Task09.cpp
@@ -3,18 +3,24 @@ using namespace std;
 
 int calculate(int a, int b, char action) {
 	switch (action) {
-		case '%': return a % b;
-		case '-': return a - b;
-		case '*': return a * b;
-		case '/': return a / b;
-		default: return a + b;
+	case '%': return a % b;
+	case '-': return a - b;
+	case '*': return a * b;
+	case '/':
+		if (b == 0) {
+			return -1;
+		}
+		else {
+			return a / b;
+		}
+	default: return a + b;
 	}
 }
 
 int main() {
-    int a, b;
-    cin >> a;
-    cin >> b;
-    cout << calculate(a, b);
-    return 0;
+	int a, b;
+	char action;
+	cin >> a >> b >> action;
+	cout << calculate(a, b, action);
+	return 0;
 }
