900d324ecb9c99888876fcd81acb31b175a95365
Nikola Svetoslavov <justsvetoslavov@gmail.com>
2022-11-02 23:15:29 +0200

    Change solution

--- IS/Sem.03/Pract.03/Task10.cpp
+++ IS/Sem.03/Pract.03/Task10.cpp
@@ -1,39 +1,44 @@
 #include <iostream>
-
 using namespace std;
 
-int main() {
-	int input;
-	cin >> input;
+int main()
+{
+	int number, digits = 0;
+	cin >> number;
 
-	int numberOfDigits = 0;
-	int inputCopy = input;
-
-	while (inputCopy != 0) {
-		inputCopy /= 10;
-		numberOfDigits++;
+	int temp = number; //copy of number
+	while (temp != 0)
+	{
+		temp /= 10;
+		digits++;
 	}
 
-	int numberOfDigitsPerHalf = numberOfDigits / 2;
-	if (numberOfDigits % 2 == 0) {
-		numberOfDigitsPerHalf--;
+	int newNumber = 0;
+	if (digits > 2)
+	{
+		int Multiplier = 1;
+		const int centralDigit = digits / 2 + 1;
+
+		for (int i = 1; i <= digits; i++)
+		{
+			if(digits % 2 == 0 && i == centralDigit - 1)
+			{
+				number /= 10;
+				continue;
+			}
+
+			if (i == centralDigit)
+			{
+				number /= 10;
+				continue;
+			}
+
+			newNumber += number % 10 * Multiplier;
+			Multiplier *= 10;
+			number /= 10;
+		}
 	}
 
-	int firstHalf = 1, secondHalf = 1;
-	for (int i = 0; i < numberOfDigits / 2 + 1; i++) {
-		firstHalf *= 10;
-	}
-	for (int i = 0; i < numberOfDigitsPerHalf; i++) {
-		secondHalf *= 10;
-	}
-
-	firstHalf = input / firstHalf;
-	secondHalf = input % secondHalf;
-
-	for (int i = 0; i < numberOfDigitsPerHalf; i++) {
-		firstHalf *= 10;
-	}
-
-	int newNumber = firstHalf + secondHalf;
-	cout << newNumber << ", " << newNumber + 1 << endl;
+	cout << newNumber << ", ";
+	cout << ++newNumber << endl;
 }
