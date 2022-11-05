ec3156f7428fee09870806a65088ff2885a400fd
Presiyana Borisova <presiana03@gmail.com>
2022-11-05 11:24:42 +0200

    Create Task02.cpp

---
+++ SI/Sem.05/Pract.05/Task02.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+using namespace std;
+
+const int SIZE = 20;
+
+void sortNumbers(int* arr, int size, bool reverse)
+{
+	if (!reverse)
+	{
+		for (int i = 0; i < size; i++)
+			for (int j = 0; j < size - i - 1; j++)
+				if (arr[j] > arr[j + 1])
+				{
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+	}
+
+	else
+	{
+		for (int i = 0; i < size; i++)
+			for (int j = 0; j < size - i - 1; j++)
+				if (arr[j] < arr[j + 1])
+				{
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+	}
+}
+
+void printArray(int* arr, int size)
+{
+	for (int i = 0; i < size; i++)
+		cout << arr[i] << " ";
+}
+
+void segregateEvenOdd(int* arr, int size, int* evenArr, int* oddArr)
+{
+	int countOfEvenNumbers = 0;
+	int countOfOddNumbers = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i] % 2 == 0)
+		{
+			evenArr[countOfEvenNumbers] = arr[i];
+			countOfEvenNumbers++;
+		}
+
+		else
+		{
+			oddArr[countOfOddNumbers] = arr[i];
+			countOfOddNumbers++;
+		}
+	}
+
+	sortNumbers(evenArr, countOfEvenNumbers,false);
+	sortNumbers(oddArr, countOfOddNumbers,true);
+
+	printArray(evenArr, countOfEvenNumbers);
+	printArray(oddArr, countOfOddNumbers);
+}
+
+void putNumbersInArray(int* arr, int size)
+{
+	for (int i = 0; i < size; i++)
+		cin >> arr[i];
+}
+
+int main()
+{
+	int arr[SIZE];
+	int evenArr[SIZE];
+	int oddArr[SIZE];
+	int size = 0;
+
+	while (size < 1 || size > 20)
+	{
+		cout << "Enter how many numbers would you want (between 1 and 20): ";
+		cin >> size;
+	}
+
+	putNumbersInArray(arr, size);
+	segregateEvenOdd(arr, size, evenArr, oddArr);
+}
