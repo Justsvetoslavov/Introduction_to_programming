f2216b3816a3aa38fac30254e0f6635ab6ea305d
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2022-11-06 20:51:02 +0200

    Update Task09.cpp

--- SI/Sem.04/Pract.04/Task09.cpp
+++ SI/Sem.04/Pract.04/Task09.cpp
@@ -15,7 +15,12 @@
 			}
 			return a / b;
 		
-		case '%': return a % b;
+		case '%': 
+			if(b == 0) {
+				std::cout << "Cannot divide by 0\n";
+				return -1;
+			}
+			return a % b;
 		
 		default:
 			std::cout << "Error\n";
