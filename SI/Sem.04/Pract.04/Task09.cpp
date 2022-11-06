c363f39f05c8221e867cb1f8f87dc63f91c23970
Nikola Svetoslavov <justsvetoslavov@gmail.com>
2022-11-06 14:27:09 +0200

    Update Task09.cpp

--- SI/Sem.04/Pract.04/Task09.cpp
+++ SI/Sem.04/Pract.04/Task09.cpp
@@ -8,10 +8,17 @@
 		
 		case '*': return a * b;
 		
-		case '/': return a / b;
+		case '/': 
+			if(b == 0) {
+				std::cout << "Cannot divide by 0\n";
+				return -1;
+			}
+			return a / b;
 		
 		case '%': return a % b;
 		
-		default: return -1;
+		default:
+			std::cout << "Error\n";
+			return -1;
 	}
 }
