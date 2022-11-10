6473fe40f7341f6620c48e5469d0c248fe3ce9f9
Silvana Nikolova <78743124+Silvana03@users.noreply.github.com>
2022-11-11 00:21:12 +0200

    Create Task03.cpp

---
+++ SI/Sem.05/Pract.05/Task03.cpp
@@ -0,0 +1,35 @@
+﻿/*
+ * Напишете програма, която превръща дадено естествено число в двоична бройна система.
+ */
+
+#include <iostream>
+using namespace std;
+
+void converter(int number)
+{
+	int arr[100];
+	int counter = 0;
+	while (number > 0)
+	{
+		if (number % 2 == 1)
+		{
+			arr[counter] = 1;
+		}
+		else
+		{
+			arr[counter] = 0;
+		}
+		counter++;
+		number /= 2;
+	}
+
+	for (int i = counter - 1; i >= 0; --i)
+	{
+		cout << arr[i];
+	}
+}
+
+int main()
+{
+	converter(87);
+}
