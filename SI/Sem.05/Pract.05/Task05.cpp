a8bdd6cf9f9c91caa790e331e8d78e36294ce27b
Silvana Nikolova <78743124+Silvana03@users.noreply.github.com>
2022-11-11 00:24:45 +0200

    Create Task05.cpp

---
+++ SI/Sem.05/Pract.05/Task05.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+bool checkIfNumberIsPolindrom(int number, int* arr, int size)
+{
+	int lastDigit;
+	for (int i = 0; i < size; ++i)
+	{
+		lastDigit = number % 10;
+		arr[i] = lastDigit;
+		number /= 10;
+	}
+
+	int reversedArr[4];
+	int counter = 0;
+	for (int i = size - 1; i >= 0; --i)
+	{
+		reversedArr[counter++] = arr[i];
+	}
+
+	for (int i = 0; i < size; ++i)
+	{
+		if (arr[i] != reversedArr[i])
+		{
+			return  false;
+		}
+	}
+
+	return  true;
+}
+
+int main()
+{
+	int number = 1221;
+	int arr[4];
+
+	checkIfNumberIsPolindrom(number, arr, 4);
+}
