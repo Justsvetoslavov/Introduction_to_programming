6f9f00e23dd03035ff4460b82d5424f5ef5eefd2
Nikola Svetoslavov <justsvetoslavov@gmail.com>
2022-11-12 13:17:54 +0200

    Fix brackets

--- SI/Sem.05/Pract.05/Task03.cpp
+++ SI/Sem.05/Pract.05/Task03.cpp
@@ -1,10 +1,9 @@
-long long toBinary(unsigned number)
+long long ToBinary(unsigned number)
 {
 	int buffer[MAX_POWTWO_SIZE];
 	int size = 0;
 
-	while (number > 0)
-	{
+	while (number > 0) {
 		buffer[size] = number % 2;
 		number /= 2;
 		size++;
@@ -12,8 +11,7 @@ long long toBinary(unsigned number)
 
 	long long convertedToBinary = 0;
 	long long multiply = 1;
-	for (unsigned i = 0; i < size; i++)
-	{
+	for (unsigned i = 0; i < size; i++) {
 		convertedToBinary += buffer[i] * multiply;
 		multiply *= 10;
 	}
