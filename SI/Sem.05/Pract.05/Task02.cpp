d44c61618edbb2e4457c42bf3a03e6cf8db056c5
Nikola Svetoslavov <justsvetoslavov@gmail.com>
2022-11-12 13:15:34 +0200

    Another solution

--- SI/Sem.05/Pract.05/Task02.cpp
+++ SI/Sem.05/Pract.05/Task02.cpp
@@ -1,84 +1,58 @@
 #include <iostream>
-using namespace std;
 
-const int SIZE = 20;
-
-void swapNumbers(int& a, int& b)
+void Swap(int &a, int &b)
 {
 	int temp = a;
 	a = b;
 	b = temp;
 }
 
-void reverseAndSortNumbers(int* arr, int size, int startIndex)
+void ResetChanges(int &index, bool &changesOccured)
 {
-	for (int i = startIndex; i < size; i++)
-		for (int j = startIndex; j < size - 1; j++)
-			if (arr[j] < arr[j + 1])
-				swapNumbers(arr[j + 1], arr[j]);
+	index = -1;
+	changesOccured = false;
 }
 
-void sortNumbers(int* arr, int size)
+bool ChangeCurrentIndex(int index, int &currentIndex)
 {
-	for (int i = 0; i < size; i++)
-		for (int j = 0; j < size - i - 1; j++)
-			if (arr[j] > arr[j + 1])
-				swapNumbers(arr[j + 1], arr[j]);
+	return currentIndex = index;
 }
 
-void printArray(int* arr, int size)
+void SortNumbers(int *arr, size_t size)
 {
-	for (int i = 0; i < size; i++)
-		cout << arr[i] << " ";
-}
+	int counter = NULL, min, max, currentIndex;
+	bool sortingEvenNumbers = true, changeOccured;
 
-void segregateEvenOdd(int* arr, int size)
-{
-	int countOfEvenNumbers = 0;
-
-	for (int i = 0; i < size - 1; i++)
+	do
 	{
-		if (arr[i] % 2 == 0)
-			continue;
+		if (sortingEvenNumbers) {
+			min = INT_MAX; //reset min
+		} else {
+			max = INT_MIN; //reset max
+		}
 
-		for (int j = i + 1; j < size; j++)
-			if (arr[j] % 2 == 0)
-				swapNumbers(arr[i], arr[j]);
-	}
+		ResetChanges(currentIndex, changeOccured);//reset change flag & index
 
-	for (int i = 0; i < size; i++)
-	{
-		if (arr[i] % 2 != 0)
-			break;
+		for (size_t i = counter; i < size; i++) {
+			if (sortingEvenNumbers) {
+				if (arr[i] % 2 == 0 && arr[i] < min) {
+					min = arr[i];
+					changeOccured = ChangeCurrentIndex(i, currentIndex);
+				}
+			} else {
+				if (arr[i] % 2 != 0 && arr[i] > max) {
+					max = arr[i];
+					changeOccured = ChangeCurrentIndex(i, currentIndex);
+				}
+			}
+		}
 
-		countOfEvenNumbers++;
-	}
-
-	sortNumbers(arr, countOfEvenNumbers);
-
-	if (size != countOfEvenNumbers)
-		reverseAndSortNumbers(arr, size, countOfEvenNumbers);
-
-	printArray(arr, size);
-}
-
-void putNumbersInArray(int* arr, int size)
-{
-	for (int i = 0; i < size; i++)
-		cin >> arr[i];
-}
-
-int main()
-{
-	int arr[SIZE];
-	int size = 0;
-
-	while (size < 1 || size > 20)
-	{
-		cout << "Enter how many numbers would you want (between 1 and 20): ";
-		cin >> size;
-	}
-
-	putNumbersInArray(arr, size);
-	segregateEvenOdd(arr, size);
+		if (!changeOccured && sortingEvenNumbers) {
+			sortingEvenNumbers = !sortingEvenNumbers;
+			changeOccured = true;
+		} else if(changeOccured) {
+			Swap(arr[counter], arr[currentIndex]);
+			counter++;
+		}
+	} while(changeOccured);
 }
