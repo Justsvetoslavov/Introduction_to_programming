2a7984e199c833cae800818411abd6fc0e5a4fb8
Nikola Svetoslavov <justsvetoslavov@gmail.com>
2022-11-12 13:11:44 +0200

    Refactor

--- SI/Sem.05/Pract.05/Task01.cpp
+++ SI/Sem.05/Pract.05/Task01.cpp
@@ -1,39 +1,38 @@
 #include <iostream>
 using namespace std;
 
-const int SIZE = 20;
+const int MAX_SIZE = 20;
 
-void findRepeatingNumber(const int* arr, int size)
+int FirstDuplicateInArray(const int *arr, int size)
 {
-    for (int i = 1; i < size; i++)
-        for (int j = i - 1; j >= 0; j--)
-            if (arr[i] == arr[j])
-            {
-                cout << "First repeating number is " << arr[i];
-
-                return;
-            }
-
-    cout << "There was no number that was repeating";
+	for (int i = 0; i < size - 1; i++) {
+		for (int j = i + 1; j < size; j++) {
+			if (arr[i] == arr[j]) {
+				return arr[i];
+            }   
+		}
+	}
+	
+	return -1;
 }
 
-void putNumbersInArray(int* arr, int size)
+void PutNumbersInArray(int* arr, int size)
 {
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < size; i++) {
         cin >> arr[i];
+    }
 }
 
 int main()
 {
-    int arr[SIZE];
+    int arr[MAX_SIZE];
     int size = 0;
 
-    while (size < 2 || size > 20)
-    {
+    while (size < 2 || size > 20) {
         cout << "Enter how many numbers would you want (between 2 and 20): ";
         cin >> size;
     }    
 
-    putNumbersInArray(arr, size);
-    findRepeatingNumber(arr, size);
+    PutNumbersInArray(arr, size);
+    FirstDuplicateInArray(arr, size);
 }
