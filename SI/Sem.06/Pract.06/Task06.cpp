211bf7442b38a217c4754c899a0a32c02bb5016f
Nikola Svetoslavov <justsvetoslavov@gmail.com>
2022-11-25 01:14:26 +0200

    Leave only task function

--- SI/Sem.06/Pract.06/Task06.cpp
+++ SI/Sem.06/Pract.06/Task06.cpp
@@ -1,27 +1,8 @@
-#include <iostream>
-using namespace std;
-void inputArray(int* arr, int size)
-{
-	for (int i = 0; i < size; i++) 
-	{
-	cin >> arr[i];
-	}
-}
 int checkUniqueNumber(const int* arr, int size)                 
 {
 	int uniqueNum = 0;
-	for (int i = 0; i < size; i++)
-	{
-	  uniqueNum ^= arr[i];
+	for (int i = 0; i < size; i++){
+		uniqueNum ^= arr[i];
 	}
 	return uniqueNum;
-}
-int main()
-{
-	const int SIZE = 7;
-	int arr[SIZE];
-	cout << "Enter seven numbers: ";
-	inputArray(arr, SIZE);
-	cout << checkUniqueNumber(arr, SIZE);
-	return 0;
 }
