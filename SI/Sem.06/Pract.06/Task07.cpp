05c95494692e5d8e43cbc06a515aceada2f6a19a
Nikola Svetoslavov <justsvetoslavov@gmail.com>
2022-11-25 01:18:10 +0200

    Refactor

--- SI/Sem.06/Pract.06/Task07.cpp
+++ SI/Sem.06/Pract.06/Task07.cpp
@@ -1,38 +1,27 @@
 #include <iostream>
-using namespace std;
-void inputArray(int* arr, int size)
-{
-	for (int i = 0; i < size; i++)
-	{
-		cin >> arr[i];
-	}
-}
+
 int powerOfTwo(int exp)
 {
 	return (1 << exp);
 }
-  void getSubNumber(const int* arr, int size, int mask)
-  {
-	 for (int i = 0; i < size; i++)
-	  {
-		 if (mask & 1)
-		 {
-			 cout << arr[i] << " ";
-		 }
-		 mask >>= 1;
-	  }  
-  }
-int main()
+
+void getSubNumber(const int* arr, int size, int mask)
 {
-	const int SIZE = 3;
-	int arr[SIZE];
-	inputArray(arr, SIZE);
-	int power = powerOfTwo(SIZE);
-	for (int i = 0; i < power; i++)
+	for (int i = 0; i < size; i++)
 	{
-		cout << "[ ";
-		getSubNumber(arr, SIZE, i);
-		cout << "]"<< " ";
+		if (mask & 1) {
+			std::cout << arr[i] << " ";
+		}
+		mask >>= 1;
+	}  
+}
+
+void printSubNumbers(const int* arr, int size)
+{
+	int length = powerOfTwo(SIZE);
+	for (int i = 0; i < length; i++) {
+		std::cout << "[ ";
+		getSubNumber(arr, size, i);
+		std::cout << "]"<< " ";
 	}
-	return 0;
 }
