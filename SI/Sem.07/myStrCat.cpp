b8d7a3bd460b1f5a8405c297e1cc2c9731f3ff3f
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2022-11-29 11:15:10 +0200

    refactor

--- SI/Sem.07/myStrCat.cpp
+++ SI/Sem.07/myStrCat.cpp
@@ -28,3 +28,36 @@ void my_strcat(char* dest, const char* source)
 
 	dest[destLength + sourceLength] = '\0';
 }
+
+// Version 1 - without strLen
+// Concatenation done in additional array
+void myStrCatV2(const char* str1, const char* str2, char* result) {
+    size_t resultIndex = 0, sourceIndex = 0;
+    while (str1[sourceIndex] != '\0') {
+        result[resultIndex++] = str1[sourceIndex++];
+    }
+    
+    sourceIndex = 0;
+    while (str2[sourceIndex] != '\0') {
+        result[resultIndex++] = str2[sourceIndex++];
+    }
+
+    result[resultIndex] = '\0';
+}
+
+// Version 2 - without strLen
+// Concatenation done in destination (as in stl strcat)
+void my_strcatV2(char* dest, const char* source)
+{
+    size_t destIndex = 0;
+    while(dest[destIndex] != '\0') {
+        destIndex++;
+    }
+
+    size_t sourceIndex = 0;
+    while (source[sourceIndex] != '\0') {
+        dest[destIndex++] = source[sourceIndex++];
+    }
+
+	dest[destIndex] = '\0';
+}
