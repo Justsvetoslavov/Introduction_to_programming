0096e71a80679edbbdd0d78c2721ab1e95854b72
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2022-11-29 11:35:54 +0200

    refactor search in text without strlen

--- SI/Sem.07/searchInText.cpp
+++ SI/Sem.07/searchInText.cpp
@@ -1,24 +1,29 @@
-bool searchInText(const char* text, const char* word) {
-    size_t textLength = myStrLen(text);
-    size_t wordLength = myStrLen(word);
+bool searchInText(const char text[], const char word[]) {
+	size_t textIndex = 0;
 
-    for (size_t i = 0; i < textLength; i++) {
-        if (text[i] == word[0]) {
-            bool contains = true;
-            i++;
-            for (size_t j = 1; j < wordLength; j++) {
-                if (text[i] == '\0' || text[i] != word[j]) {
-                    contains = false;
-                    break;
-                }
-                i++;
-            }
+	while (text[textIndex] != '\0') {
+		if (text[textIndex] == word[0]) {
+			textIndex++;
+			size_t wordIndex = 1;
+			bool contains = true;
+			while (word[wordIndex] != '\0') {
+				if (text[textIndex] != word[wordIndex]) {
+					contains = false;
+					break;
+				}
 
-            if (contains) {
-                return true;
-            }
-        }
-    }
+				textIndex++;
+				wordIndex++;
+			}
 
-    return false;
+			if (contains) {
+				return true;
+			}
+		}
+		else {
+			textIndex++;
+		}
+	}
+
+	return false;
 }
