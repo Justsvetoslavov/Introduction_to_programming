e6986e4d4c2ac455bbe5bfa9d3456a83b722b267
Eliss12 <elis.shukry@gmail.com>
2022-11-30 19:22:37 +0200

    Create Task07.cpp

---
+++ SI/Sem.07/Pract.07/Strings/Task07.cpp
@@ -0,0 +1,30 @@
+﻿
+
+#include <iostream>
+const int SIZE = 50;
+int compareStrings(const char firstString[], const char secondString[])
+{
+	int index = 0;
+	while (firstString[index] == secondString[index]) 
+	{
+		if (firstString[index] == '\0') 
+		{
+			return 0;
+		}
+		index++;
+	}
+	if (firstString[index + 1] - secondString[index + 1] < 0)
+	{
+		return -1;
+	}
+	else { return 1; }
+}
+int main()
+{
+	char firstString[SIZE];
+	char secondString[SIZE];
+	std::cin >> firstString >> secondString;
+
+	std::cout << compareStrings(firstString, secondString);
+}
+
