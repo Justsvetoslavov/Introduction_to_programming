796b1c969a367c83c9106ab62c238cc74a5e17f4
Kristian2109 <kristian.petrov1998@gmail.com>
2022-11-30 19:36:20 +0200

    Task02

--- SI/Sem.06/Pract.06/Task02.cpp
+++ SI/Sem.06/Pract.06/Task02.cpp
@@ -4,17 +4,10 @@ int countOnes(int number) {
     int numberOfOnes = 0;
     while (number != 0) {
         if ((number & 1) == 1) {
-            numberOfOnes += 1;
+            numberOfOnes++;
         }
         number >>= 1;
     }
 
     return numberOfOnes;
-}
-
-int main2()
-{
-
-    std::cout << countOnes(10000);
-    return 0;
 }
