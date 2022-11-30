0ba31758df7b9161e9513808c334b706c769b8fb
mihaela0911 <80685421+mihaela0911@users.noreply.github.com>
2022-11-30 19:06:49 +0200

    Create Task08.cpp
    Mihaela Hadzhikovakeva
    6 6MI0600211

---
+++ SI/Sem.07/Pract.07/Strings/Task08.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+const int MAX_SIZE = 100;
+
+void FlipArr(char* arr, int start)
+{
+	for (int i = start; i < MAX_SIZE; i++)
+	{
+		if (arr[i] == '\0')
+		{
+			break;
+		}
+		arr[i] = arr[i + 1];
+	}
+}
+
+void RemoveRepeating(char* str)
+{
+	for (int i = 0; i < MAX_SIZE; i++)
+	{
+		if (str[i] == '\0')
+		{
+			break;
+		}
+		for (int j = i + 1; j < MAX_SIZE; j++)
+		{
+			if (str[j] == '\0')
+			{
+				break;
+			}
+			if (str[i] == str[j])
+			{
+				FlipArr(str, j);
+
+			}
+		}
+	}
+}
+
+int main()
+{
+	char str[MAX_SIZE];
+	std::cin >> str;
+
+	RemoveRepeating(str);
+	std::cout << str;
+}
