99b0483cf792a66a2c40748239671269573a496f
Viktoria035 <115097521+Viktoria035@users.noreply.github.com>
2022-12-02 19:37:18 +0200

    Update Task02.cpp

--- SI/Sem.07/Pract.07/References/Task02.cpp
+++ SI/Sem.07/Pract.07/References/Task02.cpp
@@ -3,41 +3,43 @@ const char UPPER_BORDER = 'Z';
 const char LOWER_BORDER = 'z';
 const char UPPER_BORDER_TO_UP = 'A';
 const char LOWER_BORDER_TO_SMALL = 'a';
-void toUpper(char& a)
+bool toUpper(char& a)
 {
 	if (a >= LOWER_BORDER_TO_SMALL && a <= LOWER_BORDER)
 	{
 		a -= 32;
+		return true;
 	}
 	else
 	{
-		std::cout << false;
+		return false;
 	}
+	
 }
-void toLower(char& b)
+bool toLower(char& b)
 {
 	if (b >= UPPER_BORDER_TO_UP && b <= UPPER_BORDER)
 	{
 		b += 32;
+		return true;
+		
 	}
-	else
-	{
-		std::cout << false;
-	}
+	
 }
 int main()
 {
 	char symbol;
 	std::cin >> symbol;
-	if (symbol >= LOWER_BORDER_TO_SMALL && symbol <=LOWER_BORDER)
+	if (toUpper(symbol))
 	{
-		toUpper(symbol);
+		std::cout << symbol;
+		return 0;
+	}
+	if (toLower(symbol));
+	{
+		std::cout << symbol;
+		return 0;
 	}
 	
-	else if (symbol >= UPPER_BORDER_TO_UP && symbol <= UPPER_BORDER)
-	{
-		toLower(symbol);
-	}
-	std::cout << symbol;
 	return 0;
 }
