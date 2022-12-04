0426db2b56a45356bfa687a471915b4a879917d5
Radina Ovcharova <93722607+radi676@users.noreply.github.com>
2022-12-04 14:54:07 +0200

    Add Pract 7 Tasks 1,2,5

---
+++ SI/Sem.07/Pract.07/References/Task02.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+const char UPPER_CASE_UPPER_BORDER = 'A';
+const char LOWER_CASE_UPPER_BORDER = 'a';
+const char LOWER_CASE_LOWER_BORDER = 'z';
+const char UPPER_CASE_LOWER_BORDER = 'Z';
+const char UPPER_TO_LOWER_TRANSFORM= 32;
+
+void toUpper(char& letter)
+{
+	letter = letter - UPPER_TO_LOWER_TRANSFORM;
+}
+
+void toLower(char& letter)
+{
+	letter = letter + UPPER_TO_LOWER_TRANSFORM;
+}
+
+int main()
+{
+	char letter;
+	std::cin >> letter;
+	if (letter >= LOWER_CASE_UPPER_BORDER &&letter <= LOWER_CASE_LOWER_BORDER)
+	{
+		toUpper(letter);
+	}
+	else 
+	{
+		toLower(letter);
+	}
+
+	std::cout << letter;
+
+}
+
+//toUpper, която приема буква и я превръща в съответстващата й голяма;
+//toLower, която приема буква и я превръща в съответстващата й малка.
