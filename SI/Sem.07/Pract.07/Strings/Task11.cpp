2bf9f7e4ec2fe063c290907d339cd7c906219301
SashoVas <98760930+SashoVas@users.noreply.github.com>
2022-12-05 17:58:16 +0200

    Update Task11.cpp

--- SI/Sem.07/Pract.07/Strings/Task11.cpp
+++ SI/Sem.07/Pract.07/Strings/Task11.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 const int MaxLength = 1000;
+const int UpperCaseDifferance = 'A' - 'a';
 int GetLenght(char* str) {
     int length = 0;
     while (*str)
@@ -16,36 +17,39 @@ void Replace(char* str, int start, int end) {
         str[i] = '*';
     }
 }
+bool FindWord(char text[],char word[],int wordLength,int currentLetter) {
+    for (int j = 0; j < wordLength; j++)
+    {
+        int textDiferance = word[j] - text[currentLetter + j];
+        if (textDiferance != 0 && textDiferance != UpperCaseDifferance && textDiferance != -UpperCaseDifferance)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+void ReplacedWordsInText(char text[],char word[],int textLength,int wordLength) {
+    for (int i = 0; i < textLength; i++)
+    {
+        int textDiferance = text[i] - word[0];
+        if (textDiferance == 0 || textDiferance == UpperCaseDifferance || textDiferance == -UpperCaseDifferance)
+        {
+            if (FindWord(text,word,wordLength,i))
+            {
+                Replace(text, i, i + wordLength);
+                i += wordLength - 1;
+            }
+        }
+    }
+}
 int main()
 {
-    int UpperCaseDifferance = 'A' - 'a';
     char word[MaxLength];
     char text[MaxLength];
     cin.getline(text, MaxLength);
     cin.getline(word, MaxLength);
     int wordLength = GetLenght(word);
     int textLength = GetLenght(text);
-    for (int i = 0; i < textLength; i++)
-    {
-        int textDiferance = text[i] - word[0];
-        if (textDiferance ==0|| textDiferance == UpperCaseDifferance|| textDiferance == -UpperCaseDifferance)
-        {
-            bool isOut = false;
-            for (int j = 0; j < wordLength; j++)
-            {
-                textDiferance = word[j] - text[i + j];
-                if (textDiferance!=0&& textDiferance!=UpperCaseDifferance&& textDiferance!=-UpperCaseDifferance)
-                {
-                    isOut = true;
-                    break;
-                }
-            }
-            if (!isOut)
-            {
-                Replace(text, i, i + wordLength);
-                i += wordLength-1;
-            }
-        }
-    }
+    ReplacedWordsInText(text,word,textLength,wordLength);
     cout << text;
 }
