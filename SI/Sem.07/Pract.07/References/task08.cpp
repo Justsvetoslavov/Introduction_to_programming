266a7f4e65cb2721c9a8fd40009f94d6c3e95266
Viktoria035 <115097521+Viktoria035@users.noreply.github.com>
2022-12-06 11:14:42 +0200

    Create task08.cpp

---
+++ SI/Sem.07/Pract.07/References/task08.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+
+//Задача 8.Практикум-Да се напише функция, която приема три естествени числа a, b и k и заменя 
+//стойностите на a и b съответно с най-малкото и най-голямото число в интервата [a, b], които имат точно k различни прости делители. 
+//Ако няма такива числа в интервала, a и b остават непоменени.
+
+//Вход: 19 32 3 | Изход : 19 30 // 30 is divisible by 2, 3, 5
+//Вход : 18 32 1 | Изход : 19 31
+
+bool isPrime(int number)
+{
+	
+	for (int i = 2; i <= number/2 ; i++)
+	{
+		if (number%2==0)
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+	return true;
+}
+
+int countPrimeDivisors(int number)
+{
+	int count = 0;
+	for (int i = 2; i <= number ; i++)
+	{
+		if ((number % i==0) && isPrime(i))
+		{
+			count++;
+		}
+	}
+	return count;
+}
+void numberWithDivisors(int& numberA, int& numberB, int k)
+{
+	for (int i = numberA; i <= numberB; i++)
+	{
+		if (countPrimeDivisors(i) == k)
+		{
+			numberA = i;
+		}
+		break;
+	}
+	for (int j = numberB; j >= numberA; j--)
+	{
+		if (countPrimeDivisors(j) == k)
+		{
+			numberB = j;
+		}
+		break;
+	}
+	if (numberA > numberB)
+	{
+		numberB = numberA;
+	}
+	else
+	{
+		numberA = numberA;
+	}
+}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int k = 0;
+	std::cin >> a >> b >> k;
+	if (b < a || k > a || k > b)
+	{
+		return -1;
+	}
+	
+	numberWithDivisors(a, b, k);
+	std::cout << a << " " << b;
+	return 0;
+}
