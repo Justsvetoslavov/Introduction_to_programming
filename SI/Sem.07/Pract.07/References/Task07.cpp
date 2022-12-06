009f3204132e43fee7576360a41a3489c481dca9
Viktoria035 <115097521+Viktoria035@users.noreply.github.com>
2022-12-06 11:14:22 +0200

    Create Task07.cpp

---
+++ SI/Sem.07/Pract.07/References/Task07.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+//Да се напише програма, която по дадена програма на театър изчислява 
+//до колко часа продължава най - късното представление.От конзолата се прочита цяло число n - броят на представленията, 
+//а на следващите n реда - по 3 цели числа : час, минута на началото на преставлението и продължителността му в минути.
+//Програмата изведжда в колко часа приключва последното представление.
+const int MINUTES_IN_AN_HOUR = 60;
+const int HOURS_IN_A_DAY = 24;
+int main()
+{
+	int movies;
+	std::cin >> movies;
+	int startHour, startMinutes, durationOfMovie;
+	int latest = 0;
+	while (movies != 0)
+	{
+		std::cin >> startHour >> startMinutes >> durationOfMovie;
+		int endTime = startHour * MINUTES_IN_AN_HOUR + startMinutes + durationOfMovie;
+		if (endTime > latest)
+		{
+			latest = endTime;
+		 }
+		
+		movies--;
+		
+	}
+
+	std::cout << latest/MINUTES_IN_AN_HOUR % HOURS_IN_A_DAY<< " " << latest % MINUTES_IN_AN_HOUR;
+	return 0;
+}
