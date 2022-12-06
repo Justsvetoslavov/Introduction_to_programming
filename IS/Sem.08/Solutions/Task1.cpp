ed61c4aa071b1f8dd13d689006de06fce32734e0
Nikola Svetoslavov <justsvetoslavov@gmail.com>
2022-12-06 13:55:12 +0200

    Add solutions

---
+++ IS/Sem.08/Solutions/Task1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+int* CreateArray(size_t N)
+{
+	int* arr = new int[N];
+	
+	for (size_t i = 0; i < N; i++) {
+		arr[i] = NULL;
+    }
+
+	return arr;
+}
+
+void Initialize(int* arr, size_t size)
+{
+	for (size_t i = 0; i < size; i++) {
+		cin >> arr[i];
+    }
+}
+
+void Print(const int* arr, size_t size)
+{
+	for (size_t i = 0; i < size; i++) {
+		std::cout << arr[i] << ' ';
+    }
+}
+
+void Free(int* arr)
+{
+	delete[] arr;
+}
+
+int main()
+{
+	int N; cin >> N;
+	int *arr = CreateArray(N);
+	Initialize(arr, N);
+	Print(arr, N);
+	Free(arr);
+}
