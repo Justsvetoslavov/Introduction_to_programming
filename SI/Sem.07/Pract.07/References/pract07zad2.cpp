68a3676d8f6d1ef63fe78da677738e2d84352e4f
polinaDoncheva <115098243+polinaDoncheva@users.noreply.github.com>
2022-12-14 18:29:24 +0200

    Refactored the code and removed empty lines

--- SI/Sem.07/Pract.07/References/pract07zad2.cpp
+++ SI/Sem.07/Pract.07/References/pract07zad2.cpp
@@ -1,17 +1,12 @@
-
-
 #include <iostream>
 using namespace std;
 
 void toUpper(char& letter) 
 {
-	int difference = 'a' - 'A';
-	letter -= difference;
+	letter -= 'a' - 'A';
 }
 
 void toLower(char& letter)
 {
-	int difference = 'a' - 'A';
-	letter += difference;
+	letter += 'a' - 'A';
 }
-
