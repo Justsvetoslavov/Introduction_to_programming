10e61015daef8bb59dfd083539aafa65db89b16d
Nikola Svetoslavov <justsvetoslavov@gmail.com>
2022-12-15 01:24:34 +0200

    Fix allignment

--- SI/Sem.08/Solutions/Task1.cpp
+++ SI/Sem.08/Solutions/Task1.cpp
@@ -6,7 +6,7 @@ int* CreateArray(size_t N)
 	
 	for (size_t i = 0; i < N; i++) {
 		arr[i] = NULL;
-    }
+	}
 
 	return arr;
 }
@@ -15,14 +15,14 @@ void Initialize(int* arr, size_t size)
 {
 	for (size_t i = 0; i < size; i++) {
 		cin >> arr[i];
-    }
+	}
 }
 
 void Print(const int* arr, size_t size)
 {
 	for (size_t i = 0; i < size; i++) {
 		std::cout << arr[i] << ' ';
-    }
+	}
 }
 
 void Free(int* arr)
