712d1b1cdb28791c25cab54a6620de6d0a525174
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2022-12-16 10:58:31 +0200

    Add files via upload

---
+++ SI/Sem.08/Pract.08/FileExamples.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+
+const char UPPER_FOLDER[] = "../test.txt";
+
+const char FILE_NAME[] = "PractFiles.cpp";
+
+const int BUFFER_SIZE = 1024;
+
+int main() {
+	std::ifstream in(FILE_NAME);
+
+	if (!in.is_open()) {
+		std::cout << "Error";
+		return -1;
+	}
+	
+	while (!in.eof()) {
+		char buffer[BUFFER_SIZE];
+		in.getline(buffer, BUFFER_SIZE);
+		std::cout << buffer;
+	}
+
+	in.close();
+}
