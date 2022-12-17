fb9e722aee6c830aec966b7e3eb4623835fd17e7
Nikola Svetoslavov <justsvetoslavov@gmail.com>
2022-12-17 19:01:18 +0200

    Refactor

--- SI/Sem.07/Pract.07/Strings/Task12.cpp
+++ SI/Sem.07/Pract.07/Strings/Task12.cpp
@@ -1,66 +1,69 @@
 #include <iostream>
-using namespace std;
+
 const int MaxLength = 1000;
 const int UpperCaseDifferance = 'A' - 'a';
-int GetLenght(char* str) {
-    int length = 0;
-    while (*str)
-    {
-        length++;
-        str++;
+
+int GetLenght(const char* str)
+{
+    size_t index = 0;
+    while (str[index] != '\0') {
+        index++;
     }
-    return length;
+
+    return index;
 }
-void Replace(char* str, int start, int end) {
-    for (int i = start; i < end; i++)
-    {
+
+void Replace(char* str, size_t start, size_t end)
+{
+    for (size_t i = start; i < end; i++) {
         str[i] = '*';
     }
 }
-bool CheckIfWordIsWordAtTheStart(char text[],int position) {
+
+bool CheckIfWordIsWordAtTheStart(const char* text, size_t position)
+{
     return position == 0 || !(text[position - 1] >= 'A' && text[position - 1] <= 'z');
 }
-bool CheckIfWordIsWordAtTheEnd(char text[],int position) {
-    return text[position + 1] == '\0' || !(text[position ] >= 'A' && text[position ] <= 'z');
+
+bool CheckIfWordIsWordAtTheEnd(const char* text, size_t position)
+{
+    return text[position + 1] == '\0' || !(text[position] >= 'A' && text[position] <= 'z');
 }
-bool FindWord(char text[], char word[], int wordLength, int currentLetter) {
-    for (int j = 0; j < wordLength; j++)
-    {
-        int textDiferance = word[j] - text[currentLetter + j];
-        if (textDiferance != 0 && textDiferance != UpperCaseDifferance && textDiferance != -UpperCaseDifferance)
-        {
+
+bool FindWord(const char* text, const char* word, size_t wordLength, size_t currentLetter)
+{
+    for (size_t i = 0; i < wordLength; i++) {
+        int textDiferance = word[i] - text[currentLetter + i];
+        if (textDiferance != 0 && textDiferance != UpperCaseDifferance && textDiferance != -UpperCaseDifferance) {
             return false;
         }
     }
-    if (CheckIfWordIsWordAtTheEnd(text,currentLetter+wordLength))
-    {
-        return true;
-    }
-    return false;
+
+    return CheckIfWordIsWordAtTheEnd(text, currentLetter + wordLength);
 }
 
-void ReplacedWordInText(char text[], char word[], int textLength, int wordLength) {
-    for (int i = 0; i < textLength; i++)
-    {
+void ReplacedWordInText(char* text, const char* word, size_t textLength, size_t wordLength)
+{
+    for (size_t i = 0; i < textLength; i++) {
         int textDiferance = text[i] - word[0];
-        if ((textDiferance == 0  || textDiferance == UpperCaseDifferance || textDiferance == -UpperCaseDifferance) && CheckIfWordIsWordAtTheStart(text,i))
-        {
-            if (FindWord(text, word, wordLength, i))
-            {
+        if ((textDiferance == 0 || textDiferance == UpperCaseDifferance || textDiferance == -UpperCaseDifferance) 
+            && CheckIfWordIsWordAtTheStart(text, i)) {
+            if (FindWord(text, word, wordLength, i)) {
                 Replace(text, i, i + wordLength);
                 i += wordLength - 1;
             }
         }
     }
 }
+
 int main()
 {
     char word[MaxLength];
     char text[MaxLength];
-    cin.getline(text, MaxLength);
-    cin.getline(word, MaxLength);
+    std::cin.getline(text, MaxLength);
+    std::cin.getline(word, MaxLength);
     int wordLength = GetLenght(word);
     int textLength = GetLenght(text);
     ReplacedWordInText(text, word, textLength, wordLength);
-    cout << text;
+    std::cout << text;
 }
