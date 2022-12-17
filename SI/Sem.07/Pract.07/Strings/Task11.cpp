1c6b555eabbc6a42160ba2d76750a77a107a8702
Nikola Svetoslavov <justsvetoslavov@gmail.com>
2022-12-17 18:54:32 +0200

    Refactor

--- SI/Sem.07/Pract.07/Strings/Task11.cpp
+++ SI/Sem.07/Pract.07/Strings/Task11.cpp
@@ -1,55 +1,57 @@
 #include <iostream>
-using namespace std;
-const int MaxLength = 1000;
+
+const int MaxLength = 1024;
 const int UpperCaseDifferance = 'A' - 'a';
-int GetLenght(char* str) {
-    int length = 0;
-    while (*str)
-    {
-        length++;
-        str++;
+
+int GetLenght(const char* str)
+{
+    size_t index = 0;
+    while (str[index] != '\0') {
+        index++;
     }
-    return length;
+
+    return index;
 }
-void Replace(char* str, int start, int end) {
-    for (int i = start; i < end; i++)
-    {
+
+void Replace(char* str, size_t start, size_t end)
+{
+    for (size_t i = start; i < end; i++) {
         str[i] = '*';
     }
 }
-bool FindSubstring(char text[],char substring[],int substringLength,int currentLetter) {
-    for (int j = 0; j < substringLength; j++)
-    {
+
+bool FindSubstring(const char* text,const char* substring, size_t substringLength, size_t currentLetter)
+{
+    for (size_t j = 0; j < substringLength; j++) {
         int textDiferance = substring[j] - text[currentLetter + j];
-        if (textDiferance != 0 && textDiferance != UpperCaseDifferance && textDiferance != -UpperCaseDifferance)
-        {
+
+        if (textDiferance != 0 && textDiferance != UpperCaseDifferance && textDiferance != -UpperCaseDifferance) {
             return false;
         }
     }
     return true;
 }
-void ReplacedSubstringInText(char text[],char substring[],int textLength,int substringLength) {
-    for (int i = 0; i < textLength; i++)
-    {
+
+void ReplacedSubstringInText(char* text,const char* substring, size_t textLength, size_t substringLength) {
+    for (size_t i = 0; i < textLength; i++) {
         int textDiferance = text[i] - substring[0];
-        if (textDiferance == 0 || textDiferance == UpperCaseDifferance || textDiferance == -UpperCaseDifferance)
-        {
-            if (FindSubstring(text, substring, substringLength,i))
-            {
+        if (textDiferance == 0 || textDiferance == UpperCaseDifferance || textDiferance == -UpperCaseDifferance) {
+            if (FindSubstring(text, substring, substringLength, i)) {
                 Replace(text, i, i + substringLength);
                 i += substringLength - 1;
             }
         }
     }
 }
+
 int main()
 {
     char substring[MaxLength];
     char text[MaxLength];
-    cin.getline(text, MaxLength);
-    cin.getline(substring, MaxLength);
+    std::cin.getline(text, MaxLength);
+    std::cin.getline(substring, MaxLength);
     int substringLength = GetLenght(substring);
     int textLength = GetLenght(text);
-    ReplacedSubstringInText(text, substring,textLength, substringLength);
-    cout << text;
+    ReplacedSubstringInText(text, substring, textLength, substringLength);
+    std::cout << text;
 }
