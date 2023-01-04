3ca90beef77dcf4fbeb270c95a0d559b995e3668
Nikola Svetoslavov <justsvetoslavov@gmail.com>
2023-01-04 15:40:56 +0200

    Refactor

--- SI/Sem.09/Pract.09/Task03.cpp
+++ SI/Sem.09/Pract.09/Task03.cpp
@@ -2,16 +2,10 @@
 
 void printReverse(int num)
 {
-	if (num == 0)
-	{
+	if (num == 0) {
 		return;
 	}
+	
 	std::cout << num % 10 << " ";
 	printReverse(num / 10);
-
-}
-int main()
-{
-	printReverse(456);
-	return 0;
 }
