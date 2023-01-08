8f67523adcbda1b6758a680a9d990b52234b5c4b
Kristian2109 <kristian.petrov1998@gmail.com>
2023-01-08 15:34:21 +0200

    Task06

--- SI/Sem.09/Pract.09/Task06.cpp
+++ SI/Sem.09/Pract.09/Task06.cpp
@@ -1,32 +1,21 @@
-void Resize(int*& array, int& arraySize, int element, int index) 
-{
-	int* newArray = new int[++arraySize];
-	
-	for (size_t i = 0; i < index; i++) {
-		newArray[i] = array[i];
-	}
- 
-   	newArray[index] = element;
- 
-    	for (size_t i = index + 1; i < arraySize; i++) {
-        	newArray[i] = array[i - 1];
-    	}
- 
-    	delete[] array;
-   	array = newArray;
+#include <iostream>
+
+void swap(int& first, int& second) {
+    first = first + second;
+    second = first - second;
+    first = first - second;
 }
- 
-void AddElement(int*& array, int& arraySize, int element, int index = 0)
-{
-	if (arraySize == 0 || element >= array[arraySize - 1]) {
-        	Resize(array, arraySize, element, arraySize);
-        	return;
-    	}
- 
-    	if (element < array[index]) {
-        	Resize(array, arraySize, element, index);
-        	return;
-    	}
- 
-    	AddElement(array, arraySize, element, index + 1);
+
+void insertInt(int* arr, int len) {
+    if (arr[len - 1] >= arr[len - 2]) {
+        return;
+    }
+    swap(arr[len - 1], arr[len - 2]);
+    insertInt(arr, len - 1);
+}
+
+void print(int* arr, int len) {
+    for (int i = 0; i < len; i++) {
+        std::cout << arr[i] << " ";
+    }
 }
