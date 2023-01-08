b96d49e470aa24e37ea9718130598649f6c39414
Nikola Svetoslavov <justsvetoslavov@gmail.com>
2023-01-09 01:54:54 +0200

    Update

--- SI/Sem.09/Solutions/SearchInText.cpp
+++ SI/Sem.09/Solutions/SearchInText.cpp
@@ -2,26 +2,25 @@
 
 bool isPrefix(const char* text, const char* pattern)
 {
-  if (*pattern == '\0') {
-    return true;
-  }
+	if (*pattern == '\0') {
+		return true;
+	}
 	
-  return *text == *pattern && isPrefix(text + 1, pattern + 1);
+	return *text == *pattern && isPrefix(text + 1, pattern + 1);
 }
 
 bool searchInText(const char* text, const char* pattern)
 {
 	if (*text == '\0') {
-    return *pattern == '\0';
-  }
-
+		return *pattern == '\0';
+	}
+	
 	return isPrefix(text, pattern) || searchInText(text + 1, pattern);
 }
 
-
 int main()
 {
-  char str[] = "hello friends how are you";
+	char str[] = "hello friends how are you";
 	char pattern[] = "friends!";
 
 	std::cout << searchInText(str, pattern);
