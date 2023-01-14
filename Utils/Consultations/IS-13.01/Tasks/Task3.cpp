cf6b3210da651252b0a1842f1632a9a798ba0a93
Plamena Ilieva <92987328+plamena-ilieva@users.noreply.github.com>
2023-01-14 16:00:00 +0200

    Add solution task 3

---
+++ Utils/Consultations/IS-13.01/Tasks/Task3.cpp
@@ -0,0 +1,74 @@
+#include "pch.h"
+#include <iostream>
+
+int min(int a, int b) {
+	return a < b ? a : b;
+}
+
+int min(int a, int b, int c) {
+	return min(min(a, b), c);
+}
+
+int getMinOperations(size_t N, int* cache)
+{
+	if (N == 1)
+		return 0;
+
+	if (cache[N] != 0)
+		return cache[N];
+
+	int div3 = INT_MAX;
+	int div2 = INT_MAX;
+	int s1 = INT_MAX;
+
+	if (N % 3 == 0)
+		div3 = getMinOperations(N / 3, cache);
+	if (N % 2 == 0)
+		div2 = getMinOperations(N / 2, cache);
+	s1 = getMinOperations(N - 1, cache);
+
+	int result = 1 + min(div3, div2, s1);
+	cache[N] = result;
+	return result;
+}
+
+
+int getMinOp(int n) {
+	int* cache = new int[n + 1];
+	for (size_t i = 0; i < n+1; i++)
+	{
+		cache[i] = 0;
+	}
+
+	int result = getMinOp(n, cache);
+	delete[] cache;
+	return result;
+}
+
+//function without cache
+/*int getMinOp(int n) {
+	if (n == 1) {
+		return 0;
+	}
+
+	int div3 = INT_MAX;
+	int div2 = INT_MAX;
+	int s1 = INT_MAX;
+
+	if (n % 3 == 0) {
+		div3 = getMinOp(n / 3);
+	}
+	if (n % 2 == 0) {
+		div2 = getMinOp(n / 2);
+	}
+	s1 = getMinOp(n - 1);
+	
+	int result = min(div3, div2, s1) + 1;
+	
+	return result;
+}*/
+
+int main()
+{
+	std::cout << getMinOp(19); //4
+}
