ce24990abbec83265beaadac740d63e85e5344d2
Plamena Ilieva <92987328+plamena-ilieva@users.noreply.github.com>
2023-01-14 18:20:47 +0200

    Edit format

--- Utils/Consultations/IS-13.01/Tasks/Task4.cpp
+++ Utils/Consultations/IS-13.01/Tasks/Task4.cpp
@@ -1,40 +1,40 @@
-bool isValid(char ch) {
-	return ch >= 'A' && ch <= 'Z' || ch >= 'a' && ch <= 'z' || ch >= '0' && ch <= '9';
+bool IsValidSymbol(char ch) {
+    return ch >= 'A' && ch <= 'Z' || ch >= 'a' && ch <= 'z' || ch >= '0' && ch <= '9';
 }
 
-bool isLess(const char* w1, const char* w2) {
+bool IsLexLess(const char* w1, const char* w2) {
 
-	while (isValid(*w1) && isValid(*w2)) {
-		if (*w1 < *w2) {
-			return true;
-		}
-		if (*w1 > *w2) {
-			return false;
-		}
-		w1++;
-		w2++;
-	}
+    while (IsValidSymbol(*w1) && IsValidSymbol(*w2)) {
+        if (*w1 < *w2) {
+            return true;
+        }
+        if (*w1 > *w2) {
+            return false;
+        }
+        w1++;
+        w2++;
+    }
 
-	return !isValid(*w1);
+    return !IsValidSymbol(*w1);
 }
 
-const char* getLexMin(const char* text) {
-	const char* iter = text;
-	const char* minIter = nullptr;
+const char* GetLexMin(const char* text) {
+    const char* iter = text;
+    const char* minIter = nullptr;
 
-	while (*iter != '\0') {
-		if (isValid(*iter) && ((iter == text) || !isValid(*(iter - 1)))) {
-			if (minIter == nullptr) {
-				minIter = iter;
-			}
-			else {
-				if (isLess(iter, minIter)) {
-					minIter = iter;
-				}
-			}
-		}
-		++iter;
-	}
+    while (*iter != '\0') {
+        if (IsValidSymbol(*iter) && ((iter == text) || !IsValidSymbol(*(iter - 1)))) {
+            if (minIter == nullptr) {
+                minIter = iter;
+            }
+            else {
+                if (IsLexLess(iter, minIter)) {
+                    minIter = iter;
+                }
+            }
+        }
+        ++iter;
+    }
 
-	return minIter;
+    return minIter;
 }
