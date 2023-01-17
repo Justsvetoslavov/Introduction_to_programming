58600743b40ee2fd7f6c48924a49e3f1458759df
Margarita-Gencheva2003 <oldshaturhand@gmail.com>
2023-01-17 10:20:15 +0200

    Create Task09.cpp

---
+++ SI/Sem.09/Pract.09/Task09.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+const int MAX_NUM = 3000;
+const int MIN_NUM = 0;
+int printNLeapYears(int N, int yearNow) {
+	if (N == 0) return 0;
+	if ((N >= MAX_NUM || N <= MIN_NUM)) {
+		return -1;
+	}
+	if ((yearNow % 4 == 0 && yearNow % 100 != 0) || 
+		(yearNow % 100 == 0 && yearNow % 400 == 0)) {
+		std::cout << yearNow << " ";
+		--N;
+	}
+	return 0 + printNLeapYears(N, ++yearNow);
+}
+int main() {
+	int N = 0;
+	std::cin >> N;
+	int errorInput = printNLeapYears(N, 2000);
+	if (errorInput == -1) {
+		std::cout << "Invalid input" << std::endl;
+	}
+}
