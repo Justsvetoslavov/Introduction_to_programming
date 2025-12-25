6e132c6d21e895d84d8165daf683a49eeed788cd
Nikola Svetoslavov <justsvetoslavov@gmail.com>
2025-12-25 23:06:53 +0200

    Add Sem.09 - Demo notes

---
+++ Sem.09/Solutions/Task2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+int** CreateMatrix(size_t rows, size_t columns)
+{
+	int** matrix = new int*[rows];
+
+	for (size_t i = 0; i < rows; i++) {
+		matrix[i] = new int[columns];
+	}
+
+	for (size_t i = 0; i < rows; i++) {
+		for (size_t y = 0; y < columns; y++) {
+			matrix[i][y] = NULL; //default initialization
+		}
+	} 
+
+	return matrix;
+}
+
+void Initialize(int** matrix, size_t rows, size_t columns)
+{
+	for (size_t i = 0; i < rows; i++) {
+		for (size_t y = 0; y < columns; y++) {
+			std::cin >> matrix[i][y];
+		}
+	}
+}
+void Print(int** matrix, size_t rows, size_t columns)
+{
+	for (size_t i = 0; i < rows; i++) {
+		for (size_t y = 0; y < columns; y++) {
+			std::cout << matrix[i][y] << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+void Free(int** matrix, size_t rows)
+{
+	for (size_t i = 0; i < rows; i++) {
+		delete[] matrix[i];
+	}
+
+	delete[] matrix;
+}
